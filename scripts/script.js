adb389edf09cd82e09775d8cd733012969ced407
Grace White <s533970@nwmissouri.edu>
2023-09-19 14:50:07 -0500

    Finished Lab

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: Grace White
+    File Name: script.js
+    Date: September 16
+*/
+
+//Hamburger Menu Function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block" {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
